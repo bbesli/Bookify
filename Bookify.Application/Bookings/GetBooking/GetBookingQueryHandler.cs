f0af212fdd1ba67a458b4d0d370339d3b08e5afa
Burak Beşli <burak.besli@devoplus.com.tr>
2024-12-04 16:55:39 +0300

    init

---
+++ Bookify.Application/Bookings/GetBooking/GetBookingQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using Bookify.Application.Abstractions.Messaging;
+using Bookify.Application.Data;
+using Bookify.Domain.Abstractions;
+using Dapper;
+
+namespace Bookify.Application.Bookings.GetBooking;
+
+internal sealed class GetBookingQueryHandler : IQueryHandler<GetBookingQuery, BookingResponse>
+{
+    private readonly ISqlConnectionFactory _sqlConnectionFactory;
+
+    public GetBookingQueryHandler(ISqlConnectionFactory sqlConnectionFactory)
+    {
+        _sqlConnectionFactory = sqlConnectionFactory;
+    }
+
+    public async Task<Result<BookingResponse>> Handle(GetBookingQuery request, CancellationToken cancellationToken)
+    {
+        using var connection = _sqlConnectionFactory.CreateConnection();
+
+        var booking = await connection.QueryFirstOrDefaultAsync<BookingResponse>(
+            sql,
+            new
+            {
+                request.BookingId,
+            });
+        return booking;
+    }
+}
---
+++ Bookify.Domain/Bookings/Booking.cs
@@ -0,0 +1,148 @@
+﻿using Bookify.Domain.Abstractions;
+using Bookify.Domain.Apartments;
+using Bookify.Domain.Bookings.Events;
+using Bookify.Domain.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Domain.Bookings
+{
+    public sealed class Booking : Entity
+    {
+        private Booking(
+            Guid id,
+            Guid apartmentId,
+            Guid userId,
+            DateRange duration,
+            Money priceForPeriod,
+            Money cleaningFee,
+            Money amenitiesUpCharge,
+            Money totalPrice,
+            BookingStatus status,
+            DateTime createdOnUtc)
+            : base(id)
+        {
+            ApartmentId = apartmentId;
+            UserId = userId;
+            Duration = duration;
+            PriceForPeriod = priceForPeriod;
+            CleaningFee = cleaningFee;
+            AmenitiesUpCharge = amenitiesUpCharge;
+            TotalPrice = totalPrice;
+            Status = status;
+            CreateOnUtc = createdOnUtc;
+        }
+
+        public Guid ApartmentId { get; private set; }
+        public Guid UserId { get; private set; }
+        public DateRange Duration { get; private set; }
+        public Money PriceForPeriod { get; private set; }
+        public Money CleaningFee { get; private set; }
+        public Money AmenitiesUpCharge { get; private set; }
+        public Money TotalPrice { get; private set; }
+        public BookingStatus Status { get; private set; }
+        public DateTime CreateOnUtc { get; private set; }
+        public DateTime? ConfirmedOnUtc { get; private set; }
+        public DateTime? RejectedOnUtc { get; private set; }
+        public DateTime? CompletedOnUtc { get; private set; }
+        public DateTime? CancelledOnUtc { get; private set; }
+
+        public static Booking Reserve(
+            Apartment apartment,
+            Guid userId,
+            DateRange duration,
+            DateTime utcNow,
+            PricingService pricingService)
+        {
+            var pricingDetails = pricingService.CalculatePrice(apartment, duration);
+
+            var booking = new Booking(
+                Guid.NewGuid(),
+                apartment.Id,
+                userId,
+                duration,
+                pricingDetails.PriceForPeriod,
+                pricingDetails.CleaningFee,
+                pricingDetails.AmenitiesUpCharge,
+                pricingDetails.TotalPrice,
+                BookingStatus.Reserved,
+                utcNow);
+
+            booking.RaiseDomainEvents(new BookingReservedDomainEvent(booking.Id));
+
+            apartment.LastBookedOnUtc = utcNow;
+
+            return booking;
+        }
+
+        public Result Confirm(DateTime utcNow)
+        {
+            if(Status != BookingStatus.Reserved)
+            {
+                return Result.Failure(BookingErrors.NotReserved);
+            }
+
+            Status = BookingStatus.Confirmed;
+            ConfirmedOnUtc = utcNow;
+
+            RaiseDomainEvents(new BookingConfirmedDomainEvent(Id));
+
+            return Result.Success();
+        }
+
+        public Result Reject(DateTime utcNow)
+        {
+            if (Status != BookingStatus.Reserved)
+            {
+                return Result.Failure(BookingErrors.NotReserved);
+            }
+
+            Status = BookingStatus.Rejected;
+            ConfirmedOnUtc = utcNow;
+
+            RaiseDomainEvents(new BookingRejectedDomainEvent(Id));
+
+            return Result.Success();
+        }
+
+        public Result Complete(DateTime utcNow)
+        {
+            if (Status != BookingStatus.Reserved)
+            {
+                return Result.Failure(BookingErrors.NotReserved);
+            }
+
+            Status = BookingStatus.Completed;
+            ConfirmedOnUtc = utcNow;
+
+            RaiseDomainEvents(new BookingCompletedDomainEvent(Id));
+
+            return Result.Success();
+        }
+
+        public Result Cancel(DateTime utcNow)
+        {
+            if (Status != BookingStatus.Confirmed)
+            {
+                return Result.Failure(BookingErrors.NotConfirmed);
+            }
+
+            var currentDate = DateOnly.FromDateTime(utcNow);
+
+            if (currentDate > Duration.Start)
+            {
+                return Result.Failure(BookingErrors.AlreadyStarted);
+            }
+
+            Status = BookingStatus.Cancelled;
+            ConfirmedOnUtc = utcNow;
+
+            RaiseDomainEvents(new BookingCancelledDomainEvent(Id));
+
+            return Result.Success();
+        }
+    }
+}
